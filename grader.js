a81b51e0cac1f66ee198898b9d410ecd80f117a3
Ubuntu <ubuntu@ip-10-164-39-171.ec2.internal>
2013-07-11 19:11:46 +0000

    revised grader

--- grader.js
+++ grader.js
@@ -65,10 +65,25 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'Url to html')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if (program.url) {
+      var rest = require('restler');
+      var fs = require('fs');
+      rest.get(program.url)
+          .on('complete', function(res, resp){
+            fs.writeFileSync('tmp', res);
+            checkJson = checkHtmlFile('tmp', program.checks);
+            var outJson = JSON.stringify(checkJson, null, 4);
+            console.log(outJson);
+
+      });
+       
+    } else {
+      checkJson = checkHtmlFile(program.file, program.checks);
+      var outJson = JSON.stringify(checkJson, null, 4);
+      console.log(outJson);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
